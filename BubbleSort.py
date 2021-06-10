0c4b1e7db0c05689aa37c81dfa756ed1420a752d
AlSpidy <1301390912@qq.com>
2021-06-10 15:09:58 +0800

    update bubblesort

---
+++ BubbleSort.py
@@ -0,0 +1,25 @@
+__name__ = '__main__'
+
+
+def bubble_sort(arr):
+
+    length = len(arr)
+    for i in range(length-1):
+       for j in range(length-i-1):
+           if arr[j] > arr[j+1]:
+                tmp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = tmp
+    
+    return arr
+
+
+
+
+if __name__ == '__main__':
+    li = [3, 4, 2, 1]
+    li_sort = bubble_sort(li)
+    print(li_sort)
+
+
+pass
